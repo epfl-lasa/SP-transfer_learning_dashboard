828e88828c26ae4dd3d9d6b2b07ff2c6259d9f01
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2026-04-21 10:47:14 +0200

    Added inset sidebar from shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
 	import '../app.css';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbPage } from '$lib/components/ui/breadcrumb';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import { SidebarProvider, SidebarInset, SidebarTrigger } from '$lib/components/ui/sidebar/';
 
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen bg-background text-foreground">
-	{@render children()}
-</div>
+<SidebarProvider>
+	<AppSidebar />
+	<SidebarInset>
+		<header class="flex h-16 shrink-0 items-center gap-2">
+			<div class="flex items-center gap-2 px-4">
+				<SidebarTrigger class="-ms-1" />
+				<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+				<Breadcrumb>
+					<BreadcrumbList>
+						<BreadcrumbItem>
+							<BreadcrumbPage>Transfer Learning in Robotics</BreadcrumbPage>
+						</BreadcrumbItem>
+					</BreadcrumbList>
+				</Breadcrumb>
+			</div>
+		</header>
+		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+			{@render children()}
+		</div>
+	</SidebarInset>
+</SidebarProvider>
